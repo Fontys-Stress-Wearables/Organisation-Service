df4b362e6115e68df81921345e1f4482587c9566
roel <roelwalravenhome@gmail.com>
2022-04-19 12:03:09 +0200

    removed organization admin code

--- Organization-Service/Dtos/CreateOrganizationAdminDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Organization_Service.Dtos;
-
-public class CreateOrganizationAdminDto
-{
-    public string Name { get; set; } = "";
-}
--- Organization-Service/Dtos/UpdateOrganizationAdminDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Organization_Service.Dtos;
-
-public class UpdateOrganizationAdminDto
-{
-    public string Name { get; set; } = "";
-    public Boolean IsArchived { get; set; } = false;
-}
