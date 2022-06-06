3fcecc9dc8fe3e7972243d5af5600464b0f685a1
oererenn <onur.ereren998@gmail.com>
2022-06-06 17:33:52 +0300

    changed NATS message structure and added logs for technical health service

--- Organization-Service/Models/NatsMessage.cs
+++ Organization-Service/Models/NatsMessage.cs
@@ -4,5 +4,6 @@ public class NatsMessage<T>
 {
     public string origin { get; set; } = "organization-service";
     public string target { get; set; }
+    public string tenantId { get; set; }
     public T message { get; set; }
 }
